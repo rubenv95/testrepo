5f810b7be5331fef016330b62893a3215c76d966
Ruben Vine <ruben.vine@phs.scot>
2023-11-22 09:27:16 +0000

    a bit more code to play with

--- testest.R
+++ testest.R
@@ -1,1 +1,12 @@
-test
+# This is an example script
+
+library(tidyverse)
+
+iris |>
+  ggplot(aes(x=Sepal.Width, y=Sepal.Length, colour = Species)) +
+  geom_point()
+
+data = starwars |>
+  filter(species != "Human")
+
+table(data$gender)
